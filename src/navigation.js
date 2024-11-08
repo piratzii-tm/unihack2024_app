79f8da1c77e75ea17a8c4b7acef2d36af859d6df
Cristian Dragan <67193320+dragancristian2005@users.noreply.github.com>
2024-11-08 20:17:39 +0200

    Implemented Navigation Icons (#3)

--- src/navigation.js
+++ src/navigation.js
@@ -4,13 +4,26 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen, RecordScreen, SettingsScreen } from "./screens/";
 import StoryDetailsScreen from "./screens/StoryDetailsScreen";
 import { createStackNavigator } from "@react-navigation/native/src/__stubs__/createStackNavigator";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const TabNavigator = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Record" component={RecordScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} options={{
+                tabBarIcon: () => (
+                    <FontAwesome name="home" size={25} color="black" />
+                )
+            }}/>
+            <Tab.Screen name="Record" component={RecordScreen} options={{
+                tabBarIcon: () => (
+                    <FontAwesome name="microphone" size={25} color="black" />
+                )
+            }}/>
+            <Tab.Screen name="Settings" component={SettingsScreen} options={{
+                tabBarIcon: () => (
+                    <FontAwesome name="cog" size={25} color="black" />
+                )
+            }}/>
         </Tab.Navigator>
     )
 }
