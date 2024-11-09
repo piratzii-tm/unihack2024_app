65bc14407b2844a68674114504f1da94ca96872d
poenaruiulian <poenaruiulian2003@gmail.com>
2024-11-09 17:16:33 +0200

    Resolved imports issue

--- src/screens/app/SettingsScreen.js
+++ src/screens/app/SettingsScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Text, Button } from "react-native";
 import { KContainer } from "../../components";
-import { logout } from "../../backend/firebase/auth/logout";
+import { logout } from "../../backend";
 
 export function SettingsScreen() {
   return (
